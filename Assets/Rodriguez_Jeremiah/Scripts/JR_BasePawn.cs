b4b2aa9792a31c680ee0b7f1c457be6a2ad26fd0
maxy831 <33240985+maxy831@users.noreply.github.com>
2017-10-31 16:50:53 -0400

    tesing sub

---
+++ Assets/Rodriguez_Jeremiah/Scripts/JR_BasePawn.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JR_BasePawn : MonoBehaviour
+{
+
+    public GameObject Controller;
+    private Vector3 currentPosition;
+    private Vector3 newPosition;
+    private GameObject targetedSquare;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        currentPosition = Controller.transform.position;
+        newPosition = currentPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PositionToMove()
+    {
+        newPosition = new Vector3(targetedSquare.transform.position.x, targetedSquare.transform.position.y + 1, targetedSquare.transform.position.z);
+    }
+
+    void MovedPosition()
+    {
+        if (currentPosition != newPosition)
+        {
+            currentPosition = newPosition;
+        }
+    }
+
+    public Vector3 GetLocationOfPawn()
+    {
+        return currentPosition;
+    }
+
+    public void OnDestroy()
+    {
+        Destroy(gameObject);
+    }
+    
+}
