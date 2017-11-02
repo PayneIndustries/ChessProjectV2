a39a806dee632f718a320348f8cc32ab18f8ad57
PayneIndustries <33240309+PayneIndustries@users.noreply.github.com>
2017-11-01 20:46:03 -0400

    Revert "Revert "Revert "Merge branch 'master' of https://github.com/PayneIndustries/ChessProjectV2"""
    This reverts commit af8f44027cfb0f0c6c33316d6d610c0389036d67.

--- Assets/Rodriguez_Jeremiah/Scripts/HighlightScript.cs
+++ Assets/Rodriguez_Jeremiah/Scripts/HighlightScript.cs
@@ -3,67 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HighlightScript : MonoBehaviour
-{ 
+{
     public GameObject pawn;
-
     [SerializeField]
     GameObject[] SelectedPieces;
-    GameObject tagSelected;
-
     public Color TeamColor;
-    private bool isSelected = false;
+
 
     private void OnMouseOver()
     {
-        if (isSelected == false)
-        {
-            foreach (GameObject i in SelectedPieces)
-            {
-                i.GetComponent<Renderer>().material.color = Color.yellow;
-            }
-        }
-
-    }
-    
-    private void OnMouseExit()
-    {
-        if (isSelected == false)
-        {
-            foreach (GameObject i in SelectedPieces)
-            {
-                i.GetComponent<Renderer>().material.color = TeamColor;
-            }
-        }
-    }
-    
-    private void OnMouseDown()
-    {
-        tagSelected = GameObject.FindGameObjectWithTag("Selected");
-
-        if (tagSelected != null)
-        {
-            tagSelected.GetComponentInChildren<Renderer>().material.color = TeamColor;
-            tagSelected.tag = "Untagged";
-        }
-
-        pawn.tag = "Selected";
-
         foreach (GameObject i in SelectedPieces)
         {
             i.GetComponent<Renderer>().material.color = Color.yellow;
         }
-
     }
 
-    void Update()
+    private void OnMouseExit()
     {
-        if(pawn.tag == "Selected")
+        foreach (GameObject i in SelectedPieces)
         {
-            isSelected = true;
-        }
-        else if (pawn.tag == "Untagged")
-        {
-            isSelected = false;
+            i.GetComponent<Renderer>().material.color = TeamColor;
         }
     }
 }
